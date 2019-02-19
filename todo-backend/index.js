d39ce6e93a80f382a3d975f4662311de74cafa83
chikkupa <chikku@hashin.in>
2019-02-19 14:06:47 +0530

    CORS enabled

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -4,6 +4,15 @@ const app = express();
 const todos = require('./routes/todo');
 
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.use('/api/todos', todos);
 
