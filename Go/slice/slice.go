6cdfc1f45482802a44002b2ed2146eef6eafe3eb
chikkupa <chikku@hashin.in>
2019-03-03 21:24:45 +0530

    Slice example added

---
+++ Go/slice/slice.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := make([]int, 3)
+
+	fmt.Println(slice)
+	slice[1] = 10
+	fmt.Println(slice)
+	slice = append(slice, 46)
+	fmt.Println(slice)
+	slice = append(slice, 36, 27)
+	fmt.Println(slice)
+	newSlice := slice[2:5]
+	fmt.Println(newSlice)
+	newSlice = slice[:5]
+	fmt.Println(newSlice)
+	newSlice = slice[2:]
+	fmt.Println(newSlice)
+	newSlice = slice[:]
+	fmt.Println(newSlice)
+	newSlice[2] = 79
+	fmt.Println(newSlice)
+	fmt.Println(slice)
+	newSlice = slice
+	fmt.Println(newSlice)
+
+	strSlice := make([]string, 3)
+	strSlice[1] = "Slice Element 1"
+	fmt.Println(strSlice)
+	strSlice[0] = "Slice Element 0"
+	fmt.Println(strSlice)
+}
