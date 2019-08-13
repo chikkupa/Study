9b9b4bc5334d5843f01c369f56121f942b85f2d0
chikkupa <chikku@hashin.in>
2019-08-13 18:35:22 +0530

    Changed inline css

--- Typescript/tic_tac_toe/tic_tac_toe.js
+++ Typescript/tic_tac_toe/tic_tac_toe.js
@@ -1,10 +1,7 @@
 var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
@@ -12,7 +9,8 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var Cell = /** @class */ (function () {
-    function Cell() {
+    function Cell(value) {
+        this.value = value;
         this.value = Cell.EMPTY;
         this.locked = false;
     }
