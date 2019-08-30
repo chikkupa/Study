1e83286aba0c72652f414a1c3b778f3017565021
chikkupa <chikku@hashin.in>
2019-08-30 16:29:09 +0530

    Tic Tac Toe Game: React

--- ReactJS/HelloReact/public/app.jsx
+++ ReactJS/HelloReact/public/app.jsx
@@ -1,4 +1,15 @@
+var Greeter = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <h1>Test content</h1>
+                <p>This is a paragraph</p>
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <h1>Test content</h1>,
+    <Greeter/>,
     document.getElementById("app")
 );
