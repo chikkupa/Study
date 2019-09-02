d1192443832d8abcf7b727cf99fb5b897af89183
chikkupa <chikku@hashin.in>
2019-09-03 00:11:21 +0530

    ReactJS component

---
+++ ReactJS/components/src/ApprovalCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ApprovalCard = () => {
+    return(
+        <div className="ui card">
+            <div className="content">Are you sure?</div>
+            <div className="extra container">
+                <div className="ui two buttons">
+                    <div className="ui basic green button"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- ReactJS/components/src/index.js
+++ ReactJS/components/src/index.js
@@ -6,9 +6,21 @@ import CommentDetail from './CommentDetail'
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4 PM" avatar={faker.image.avatar()} content="New blog post" />
-            <CommentDetail author="Sarath" timeAgo="Today at 2 AM" avatar={faker.image.avatar()} content="This is a blog post" />
-            <CommentDetail author="John" timeAgo="Yesterday at 8:30 PM" avatar={faker.image.avatar()} content="This is my blog" />
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 4 PM" 
+                avatar={faker.image.avatar()} 
+                content="New blog post" />
+            <CommentDetail 
+                author="Sarath" 
+                timeAgo="Today at 2 AM" 
+                avatar={faker.image.avatar()} 
+                content="This is a blog post" />
+            <CommentDetail 
+                author="John" 
+                timeAgo="Yesterday at 8:30 PM" 
+                avatar={faker.image.avatar()} 
+                content="This is my blog" />
         </div>
     );
 }
