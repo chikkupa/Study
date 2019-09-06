2f610c0e2cfa897bb479b2dccdef3b6952f5f5d2
chikkupa <chikku@hashin.in>
2019-09-06 20:04:20 +0530

    ReactJS: pics

--- ReactJS/pics/src/components/App.js
+++ ReactJS/pics/src/components/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{ marginTop: '10px' }}>
-            <SearchBar />
-        </div>
-    );
+class App extends React.Component {
+    onSearchSubmit = (term)=> {
+        //console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params : {
+                query : term
+            },
+            headers : {
+                Authorization: 'Client-ID 5cf6d9b48b7234d9eebc4a72f3943a36f4250abefe5e7f8d1baca4f4b565d513'
+            }
+        });
+    }
+    render (){
+        return (
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
 }
 
 export default App
